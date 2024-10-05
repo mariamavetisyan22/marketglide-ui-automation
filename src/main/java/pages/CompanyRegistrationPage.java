635be5599f045022eaeea06949ab8e707ace1da3
mariam <mariam.avetis@outlook.com>
2024-10-06 01:31:45 +0400

    Initial commit

---
+++ src/main/java/pages/CompanyRegistrationPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BaseUtils;
+
+public class InvestorRegistrationPage {
+
+    private BaseUtils baseUtils;
+    private WebDriver driver;
+
+    @FindBy(className = "Title")
+    private WebElement pageTitle;
+
+    public InvestorRegistrationPage(final WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+        baseUtils = new BaseUtils(driver);
+    }
+
+    public String getInvestorRegistrationPageTitle() {
+        return baseUtils.getText(pageTitle);
+    }
+}
